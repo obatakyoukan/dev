393262e2a35d880646b4eea573e06f1d4889519d
obatakyoukan <oba-kyoukan@eis.hokudai.ac.jp>
2020-02-28 00:45:28 +0900

    reading-recoder project

---
+++ vue/reading-recoder/src/components/BookInfo.vue
@@ -0,0 +1,73 @@
+<template>
+<div class="clearfix" :class="{ linkable }" @click="onclick">
+ <div class="image"><img :src="book.image" /> </div>
+ <div class="details">
+  <ul>
+   <li v-if="index">{{ index }}. </li>
+   <li> Title:     {{book.title}} </li>
+   <li> Price:     {{book.price}} </li>
+   <li> Writer:    {{book.author}}</li>
+   <li> Publisher: {{book.publisher}} </li>
+   <li> Published: {{book.published}} </li>
+  </ul>
+ </div>
+</div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import { UPDATE_CURRENT } from '@/store/mutation-types'
+
+export default {
+ name: 'book-info',
+ props: {
+  index: { type: Number },
+  linkable: { type: Boolean, default: false },
+  book: { type: Object }
+ },
+ methods: {
+  ...mapActions([UPDATE_CURRENT]),
+  onclick() {
+   if (this.linkable) {
+    this[UPDATE_CURRENT](this.book)
+    this.$router.push('/form')
+   }
+  }
+ }
+}
+</script>
+
+<style scoped>
+.linkable:hover {
+    cursor: pointer;
+    background-color: #ff9;
+}
+
+.image {
+  float:left;
+}
+
+.image img {
+  height: 180px;
+}
+
+.details {
+  float: left;
+}
+
+.details li {
+  list-style-type: none;
+  text-align: left;
+}
+
+.clearfix {
+  width: 100%;
+  margin-bottom: 10px;
+}
+
+.clearfix:after {
+  content: "";
+  display: block;
+  clear: both;
+}
+</style>
